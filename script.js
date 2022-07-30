8bc68c18171f3e310753604c4069157eb92e1895
Rescor <rescorent@gmail.com>
2022-07-30 13:51:52 +0200

    Code refactoring

--- script.js
+++ script.js
@@ -1,53 +1,48 @@
-const arrivals = data.arrivals;
-const departures = data.departures;
-const gateElem = document.getElementById("gate")
-console.log(departures)
-const arrivalsBtn = document.getElementById("arrivalsBtn");
+const arrivals      = data.arrivals;
+const departures    = data.departures;
+const gateElem      = document.getElementById("gate")
+const dataElem      = document.getElementById("data");
+const arrivalsBtn   = document.getElementById("arrivalsBtn");
 const departuresBtn = document.getElementById("departuresBtn");
-const dataElem = document.getElementById("data");
 
-for (let i = 0; i < arrivals.length; i++) {
-    const time = new Date(arrivals[i].sched).toLocaleTimeString();
-    const timeStr = time.toString();
-    const editedTimeStr = timeStr.slice(0, -3);
-    dataElem.innerHTML += `<tr><td>${arrivals[i].fnr}</td>
-                            <td>${arrivals[i].alname}</td>
-                            <td>${arrivals[i].apname}</td>
-                            <td>${editedTimeStr}</td>
-                            <td>${arrivals[i].status}</td>
-                            <td>${arrivals[i].terminal}</td></tr>`
-}
+function showArrivals() {
+    dataElem.innerHTML      = "";
+    gateElem.style.display  = "none";
 
-
-arrivalsBtn.onclick = () => {
-    dataElem.innerHTML = "";
-    gateElem.style.display = "none";
     for (let i = 0; i < arrivals.length; i++) {
-        const time = new Date(arrivals[i].sched).toLocaleTimeString();
-        const timeStr = time.toString();
-        const editedTimeStr = timeStr.slice(0, -3);
-        dataElem.innerHTML += `<tr><td>${arrivals[i].fnr}</td>
+        const time = getFormattedTime(arrivals[i].sched);
+        dataElem.innerHTML  += `<tr><td>${arrivals[i].fnr}</td>
                                 <td>${arrivals[i].alname}</td>
                                 <td>${arrivals[i].apname}</td>
-                                <td>${editedTimeStr}</td>
+                                <td>${time}</td>
                                 <td>${arrivals[i].status}</td>
                                 <td>${arrivals[i].terminal}</td></tr>`
     }
 }
 
-departuresBtn.onclick = () => {
-    dataElem.innerHTML = "";
-    gateElem.style.display = "";
+function showDepartures() {
+    dataElem.innerHTML      = "";
+    gateElem.style.display  = "table-cell";
+    
     for (let i = 0; i < departures.length; i++) {
-        const time = new Date(departures[i].sched).toLocaleTimeString();
-        const timeStr = time.toString();
-        const editedTimeStr = timeStr.slice(0, -3);
-        dataElem.innerHTML += `<tr><td>${departures[i].fnr}</td>
+        const time = getFormattedTime(departures[i].sched);
+        dataElem.innerHTML  += `<tr><td>${departures[i].fnr}</td>
                                 <td>${departures[i].alname}</td>
                                 <td>${departures[i].apname}</td>
-                                <td>${editedTimeStr}</td>
+                                <td>${time}</td>
                                 <td>${departures[i].status}</td>
                                 <td>${departures[i].terminal}</td>
                                 <td>${departures[i].gate}</td></tr>`
     }
 }
+
+function getFormattedTime(date) {
+    const time          = new Date(date).toLocaleTimeString();
+    const editedTimeStr = time.slice(0, -3);
+    return editedTimeStr;
+}
+
+arrivalsBtn.onclick     = () => showArrivals();
+departuresBtn.onclick   = () => showDepartures();
+
+showArrivals();
